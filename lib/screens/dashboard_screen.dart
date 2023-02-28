8577016fe11edb7b040ef6f6b807b75919543929
DulakshiAmarasooriya <dulakshidilhani8@gmail>
2023-02-28 13:45:12 +0530

    The 'Best of 2019' Bar

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -17,81 +17,119 @@ class DashboardScreen extends StatelessWidget {
           )
         ],
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  'Recommended',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Recommended',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                TextButton(
-                  child: const Text('View All'),
-                  onPressed: () {},
-                )
-              ],
+                  TextButton(
+                    child: const Text('View All'),
+                    onPressed: () {},
+                  )
+                ],
+              ),
             ),
-          ),
-          Container(
-            height: 200,
-            child: ListView(
-              scrollDirection: Axis.horizontal,
-              children: [
-                HorizontalList(),
-              ],
+            Container(
+              height: 250,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                // ignore: prefer_const_literals_to_create_immutables
+                children: [
+                  const HorizontalList(),
+                  const HorizontalList(),
+                  const HorizontalList(),
+                  const HorizontalList(),
+                ],
+              ),
             ),
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  'Best of 2019',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
+            const SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Best of 2019',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                TextButton(
-                  child: const Text('View All'),
-                  onPressed: () {},
-                )
-              ],
+                  TextButton(
+                    child: const Text('View All'),
+                    onPressed: () {},
+                  )
+                ],
+              ),
             ),
-          ),
-          Container(
-            height: 500,
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: ListView(
-              children: [
-                Column(
-                  children: [
-                    Card(
-                      elevation: 5,
-                      child: Row(
-                        children: [
-                          Container(
-                            decoration: BoxDecoration(),
-                          )
-                        ],
+            Container(
+              height: 500,
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: ListView(
+                children: [
+                  Column(
+                    children: [
+                      Card(
+                        elevation: 5,
+                        child: Row(
+                          children: [
+                            Container(
+                              height: 150,
+                              width: 100,
+                              decoration: const BoxDecoration(
+                                borderRadius: BorderRadius.only(
+                                  bottomLeft: Radius.circular(5),
+                                  topLeft: Radius.circular(5),
+                                ),
+                                image: DecorationImage(
+                                  fit: BoxFit.cover,
+                                  image: NetworkImage(
+                                      'https://imageio.forbes.com/blogs-images/scottmendelson/files/2019/04/screencapture-file-C-Users-JckNa-OneDrive-Desktop-b-avengersendgame-horizontal-friday-17793-04d92fbf-webp-2019-04-23-08_14_14.jpg'),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              padding: const EdgeInsets.all(10),
+                              height: 150,
+                              child: Column(
+                                children: [
+                                  const Text(
+                                    'Avengers End Game',
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  Container(
+                                    width: 200,
+                                    child: const Text(
+                                        'After Thanos, an intergalactic warlord, disintegrates half of the universe, the Avengers must reunite and assemble again to reinvigorate their trounced allies and restore balance.'),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    )
-                  ],
-                )
-              ],
+                    ],
+                  )
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/widget/horizontal_list.dart
+++ lib/widget/horizontal_list.dart
@@ -8,35 +8,37 @@ class HorizontalList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 10),
-      width: 160,
-      child: Column(
-        children: [
-          Card(
-            elevation:10,
-            child: Container(
-              height: 200,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(5),
-                image: const DecorationImage(
-                  fit: BoxFit.cover,
-                  image:
-                      NetworkImage('https://images.app.goo.gl/o4GXV5rkX6HFviVs9'),
+      width: 170,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Card(
+              elevation: 10,
+              child: Container(
+                height: 200,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  image: const DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage(
+                        'https://imageio.forbes.com/blogs-images/scottmendelson/files/2019/04/screencapture-file-C-Users-JckNa-OneDrive-Desktop-b-avengersendgame-horizontal-friday-17793-04d92fbf-webp-2019-04-23-08_14_14.jpg'),
+                  ),
                 ),
               ),
             ),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          const Text(
-            'Star Wars',
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
+            const SizedBox(
+              height: 10,
             ),
-            textAlign: TextAlign.center,
-          ),
-        ],
+            const Text(
+              'Star Wars',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
       ),
     );
   }
